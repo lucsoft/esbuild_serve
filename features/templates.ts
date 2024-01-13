84665ba2a5d406959fd96bdb2e2bc0259be9a6ca
lucsoft <lucsoft@users.noreply.github.com>
2024-01-13 00:17:19 +0000

    Update dependencies

--- features/templates.ts
+++ features/templates.ts
@@ -1,8 +1,8 @@
-import { copySync } from "https://deno.land/std@0.211.0/fs/copy.ts";
-import { emptyDirSync } from "https://deno.land/std@0.211.0/fs/empty_dir.ts";
+import { copySync } from "https://deno.land/std@0.212.0/fs/copy.ts";
+import { emptyDirSync } from "https://deno.land/std@0.212.0/fs/empty_dir.ts";
 import { Plugin } from "https://deno.land/x/esbuild@v0.19.11/mod.js";
 import { ensureNestedFolderExists } from "./filesystem.ts";
-import { assert } from "https://deno.land/std@0.211.0/assert/mod.ts";
+import { assert } from "https://deno.land/std@0.212.0/assert/mod.ts";
 
 export function provideTemplate(id: string, outdir: string, template: string, c: TemplateConfig) {
     if (id.endsWith("/"))
