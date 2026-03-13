e3fcc43f8e422c298f4325044e610111f25abc76
lucsoft <11093382+lucsoft@users.noreply.github.com>
2026-03-13 00:29:28 +0000

    Update dependencies

--- types.ts
+++ types.ts
@@ -1,4 +1,4 @@
-import { Loader, Plugin } from "https://deno.land/x/esbuild@v0.27.3/mod.js";
+import { Loader, Plugin } from "https://deno.land/x/esbuild@v0.27.4/mod.js";
 
 export type ServeConfig = {
     /** default 1337 */
